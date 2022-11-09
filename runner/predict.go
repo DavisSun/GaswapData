47a2cc50e717305ab48a3927eb640bddaecd5e27
XIAOBING\HaoDaWei <haodawei@xiaoice.com>
2022-11-09 20:34:43 +0800

    MOD: Temp update

--- runner/predict.go
+++ runner/predict.go
@@ -20,22 +20,35 @@ func Start() {
 	scheduler.StartAsync()
 }
 
-func Predict(blockNumber uint64) {
+func Predict(blockNumber uint64) []*internal.GasPricePredictionRecord {
 	transacions := convertTxPoolContentToTransactions(crawlers.GetTxPoolContent())
+	result := make([]*internal.GasPricePredictionRecord, 0)
+	var totalGasLimit uint64
+	var gasLimit uint64
+	blockHeight := blockNumber + 1
+	transactionsSumCount := 0
 	for _, tx := range transacions {
+
 		println(tx.FirstGasPrice)
 	}
+
 }
 
 // Only obtain pending
-func convertTxPoolContentToTransactions(response *internal.TxpoolContentResponse) internal.Transactions {
+func convertTxPoolContentToTransactions(response *internal.TxpoolContentResponse, nextBlockBaseFee uint64) internal.Transactions {
 	pending := response.Pending
 	result := internal.Transactions{}
 	for from, poolTxs := range pending {
 		var firstGasPrice uint64
 		for nonce, txDetail := range poolTxs {
+			gasPrice := txDetail.GasTipCap.ToInt().Uint64()
+
+			if txDetail.Type == 2 {
+				gasPrice += nextBlockBaseFee
+
+			}
 			if firstGasPrice == 0 {
-				firstGasPrice = txDetail.GasPrice.ToInt().Uint64()
+				firstGasPrice = gasPrice
 			}
 			tx := internal.Transaction{
 				From:          from,
